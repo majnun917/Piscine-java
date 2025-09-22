c8eb90b277d478ff9632c916d7886a8ba6f4d8e4
ndieye <dieye.ndiasse17@gmail.com>
2025-09-22 12:10:33 +0100

    StringConcat done !

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,6 +2,8 @@ public class StringConcat {
     public static String concat(String s1, String s2) {
         if (s1 == null && s2 == null){
             return null;
+        }else if (s1 == null || s2 == null) {
+            return "il pleut";
         }
         return s1 + s2;
     }

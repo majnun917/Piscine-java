3af23dcb5a0b108ee79de04a8860bf7660ca82a3
ndieye <dieye.ndiasse17@gmail.com>
2025-09-22 11:37:28 +0100

    StringLength done

---
+++ StringLength/StringLength.java
@@ -0,0 +1,9 @@
+public class StringLength {
+     public static int getStringLength(String s) {
+        int str_len = 0;
+        while (str_len <  s.length()) {
+            str_len ++;
+        }
+        return str_len;
+    }
+}

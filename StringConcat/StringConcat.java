789e36857010e334162143fd8b1d81507dd475fe
ndieye <dieye.ndiasse17@gmail.com>
2025-09-22 12:02:37 +0100

    StringConcat done

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,10 @@
+package StringConcat;
+
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            return "il pleut";
+        }
         return s1 + s2;
     }
 }

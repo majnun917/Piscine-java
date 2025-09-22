0cb5100dc005a4a4e97a0c3aacd11f05c45a98f9
ndieye <dieye.ndiasse17@gmail.com>
2025-09-22 16:34:27 +0100

    Palindrome done

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,15 +1,18 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        //int l = 0;
-        int i = s.length()-1;
+        // int l = 0;
+        int i = s.length() - 1;
         String right = "";
-        while (i >=0){
-            right += s.charAt(i);
+        while (i >= 0) {
+            right += s.toLowerCase().charAt(i);
+            // System.out.println("RR: " + right);
             i--;
         }
-        if (s.equals(right)){
+        s = s.toLowerCase();
+        // System.out.println("SS: " + s);
+        if (s.equals(right)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }

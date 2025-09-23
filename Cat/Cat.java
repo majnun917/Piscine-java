a7ee2c2ddcb9945a2d9a1b30a35949774389d194
ndieye <dieye.ndiasse17@gmail.com>
2025-09-23 18:03:19 +0100

    Cat done

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -1,18 +1,21 @@
 import java.io.*;
 
 public class Cat {
-    public static void cat(String[] args) {
-        if (args.length == 0) {
-            return;
-        }
-        for (String filePath : args) {
-            try (FileInputStream fis = new FileInputStream(filePath)) {
-                byte[] fileBytes = fis.readAllBytes();
-                String fileContent = new String(fileBytes);
-                System.out.println(fileContent);
-            } catch (IOException e) {
-                System.err.println("Erreur");
+
+public static void cat(String[] args) {
+    if (args.length == 0) {
+        return;
+    }
+    
+    for (String arg : args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(arg))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + arg);
         }
     }
+}
 }

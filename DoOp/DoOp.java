5e75fbe31d15a7355fd70d143e1cdbbae9917efb
ndieye <dieye.ndiasse17@gmail.com>
2025-09-23 13:21:14 +0100

    DoOp done !

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,30 +1,38 @@
 public class DoOp {
     public static String operate(String[] args) {
-        if (args.length == 0){
-            return "it depend on the input.";
+        if (args.length != 3){
+            return "Error";
         }
-
         int res = 0;
         int a1 = Integer.parseInt(args[0]);
+        String op = args[1];
         int a3 = Integer.parseInt(args[2]);
-        if (args[1] == "+"){
-            res = a1 + a3;
-        }else if (args[1] == "-") {
-            res = a1 - a3;
-        }else if (args[1] == "*"){
-            res = a1 * a3;
-        }else if (args[1] == "/"){
-            if (a3 == 0){
+
+        switch(op){
+            case "+":
+                res = a1 + a3;
+                break;
+            case "-":
+                res = a1 - a3;
+                break;
+            case "*":
+                res = a1 * a3;
+                break;
+            case "/":
+                if (a3 == 0){
                 return "Error";
-            }
-            res = a1 / a3;
-        }else if (args[1] == "%"){
-            if (a3 == 0){
+                }
+                res = a1 / a3;
+                break;
+            case "%":
+                if (a3 == 0){
                 return "Error";
-            }
-            res = a1 % a3;
-        }else{
-            return Integer.toString(res);
+                }
+                res = a1 % a3;
+                break;
+            default:
+                return "Error";
+
         }
         return Integer.toString(res);
     }

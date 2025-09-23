6214506a1529239a2ada00ab6c8c2d984d7dec4a
ndieye <dieye.ndiasse17@gmail.com>
2025-09-23 19:13:44 +0100

    Capitalize done !

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -1,26 +1,35 @@
+
 import java.io.*;
 
 public class Capitalize {
+
     public static void capitalize(String[] args) throws IOException {
-        if (args.length != 2) {
-            return;
-        }
-        try (FileInputStream fis = new FileInputStream(args[0]); FileOutputStream fos = new FileOutputStream(args[1])) {
-            int c;
-            boolean isFirstCharOfWord = true;
-            boolean hasWrittenChar = false;
-            while ((c = fis.read()) != -1) {
-                if (Character.isWhitespace(c)) {
-                    if (hasWrittenChar && !isFirstCharOfWord) {
-                        fos.write(' ');
-                    }
-                    isFirstCharOfWord = true;
-                } else {
-                    fos.write(isFirstCharOfWord ? Character.toUpperCase(c) : Character.toLowerCase(c));
-                    isFirstCharOfWord = false;
-                    hasWrittenChar = true;
+    if (args.length != 2) {
+        return;
+    }
+    
+    try (FileInputStream fis = new FileInputStream(args[0]); 
+         FileOutputStream fos = new FileOutputStream(args[1])) {
+        
+        int c;
+        boolean isFirstCharOfWord = true;
+        boolean space = false; 
+        
+        while ((c = fis.read()) != -1) {
+            if (Character.isWhitespace(c)) {
+                if (!isFirstCharOfWord) {
+                    space = true;
                 }
+                isFirstCharOfWord = true;
+            } else {
+                if (space) {
+                    fos.write(' ');
+                    space = false;
+                }
+                fos.write(isFirstCharOfWord ? Character.toUpperCase(c) : Character.toLowerCase(c));
+                isFirstCharOfWord = false;
             }
         }
     }
+}
 }

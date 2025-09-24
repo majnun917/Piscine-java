50f835426ee71f2a6a754ce7d75bc7e31625b6ab
ndieye <dieye.ndiasse17@gmail.com>
2025-09-24 13:24:02 +0100

    Capitalize Done !

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -1,35 +1,35 @@
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Capitalize {
 
     public static void capitalize(String[] args) throws IOException {
-    if (args.length != 2) {
-        return;
-    }
-    
-    try (FileInputStream fis = new FileInputStream(args[0]); 
-         FileOutputStream fos = new FileOutputStream(args[1])) {
-        
-        int c;
-        boolean isFirstCharOfWord = true;
-        boolean space = false; 
-        
-        while ((c = fis.read()) != -1) {
-            if (Character.isWhitespace(c)) {
-                if (!isFirstCharOfWord) {
-                    space = true;
+        if (args.length != 2) {
+            return;
+        }
+
+        try (Reader r = new InputStreamReader(new FileInputStream(args[0]), StandardCharsets.UTF_8);
+             Writer w = new OutputStreamWriter(new FileOutputStream(args[1]), StandardCharsets.UTF_8)) {
+
+            int c;
+            boolean isFirstCharOfWord = true;
+            boolean space = false;
+            while ((c = r.read()) != -1) {
+                if (Character.isWhitespace(c)) {
+                    if (!isFirstCharOfWord) {
+                        space = true;
+                    }
+                    isFirstCharOfWord = true;
+                } else {
+                    if (space) {
+                        w.write(' ');
+                        space = false;
+                    }
+                    w.write(isFirstCharOfWord ? Character.toUpperCase((char) c) : Character.toLowerCase((char) c));
+                    isFirstCharOfWord = false;
                 }
-                isFirstCharOfWord = true;
-            } else {
-                if (space) {
-                    fos.write(' ');
-                    space = false;
-                }
-                fos.write(isFirstCharOfWord ? Character.toUpperCase(c) : Character.toLowerCase(c));
-                isFirstCharOfWord = false;
             }
         }
     }
-}
 }

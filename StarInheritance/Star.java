9a31b7a3085fbc74ea4d1c7aced473d5ceff7505
ndieye <dieye.ndiasse17@gmail.com>
2025-09-24 19:14:50 +0100

    StarInheritance Done!

--- StarInheritance/Star.java
+++ StarInheritance/Star.java
@@ -8,14 +8,4 @@ public class Star extends CelestialObject {
     public void setMagnitude(double magnitude) {
         this.magnitude = magnitude;
     }
-
-    public static void main(String[] args) {
-        Star star = new Star();
-        
-        System.out.println(star.getName());
-        System.out.println(star.getX());
-        System.out.println(star.getY());
-        System.out.println(star.getZ());
-        System.out.println(star.getMagnitude());
-    }
 }

d4f16c35b02dd114ece41203a09381edbd01325f
ndieye <dieye.ndiasse17@gmail.com>
2025-09-24 14:33:01 +0100

    FileSearch Done!

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,34 @@
+
+import java.io.*;
+
+public class FileSearch {
+
+    public static String searchFile(String fileName) {
+        File documentsFolder = new File("document");
+        //System.out.println(documentsFolder.getAbsolutePath()); 
+        return searchInDirectory(documentsFolder, fileName);
+    }
+
+    private static String searchInDirectory(File directory, String fileName) {
+        if (!directory.exists() || !directory.isDirectory()) {
+            return null;
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null) {
+            return null;
+        }
+
+        for (File file : files) {
+            if (file.isFile() && fileName.equalsIgnoreCase(file.getName())) {
+                return file.getAbsolutePath();
+            } else if (file.isDirectory()) {
+                String result = searchInDirectory(file, fileName);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}

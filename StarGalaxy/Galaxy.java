88799232caf3b6624eb0e57e8e66e2ca9fb1155b
Ndiasse Dieye <you@example.com>
2025-09-26 18:44:01 +0100

    add StarGalaxy done!

--- StarGalaxy/Galaxy.java
+++ StarGalaxy/Galaxy.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Galaxy {
-    private final List<CelestialObject> celestialObjects;
+    private List<CelestialObject> celestialObjects;
 
     public Galaxy() {
         this.celestialObjects = new ArrayList<>();

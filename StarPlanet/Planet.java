3f332adb2f47737ef8d99accf937acc73eb5b941
Ndiasse Dieye <you@example.com>
2025-09-26 18:35:35 +0100

    add StarPlanet done!

--- StarPlanet/Planet.java
+++ StarPlanet/Planet.java
@@ -1,13 +1,16 @@
+import java.util.Objects;
+
 public class Planet extends CelestialObject {
     private Star centerStar;
 
     public Planet() {
+        super();
         this.centerStar = new Star();
     }
 
-    public Planet(String name, double x, double y, double z, Star centerStar) {
+    public Planet(String name, double x, double y, double z, Star centeStar) {
         super(name, x, y, z);
-        this.centerStar = centerStar;
+        this.centerStar = centeStar;
     }
 
     public Star getCenterStar() {
@@ -18,46 +21,27 @@ public class Planet extends CelestialObject {
         this.centerStar = centerStar;
     }
 
-      @Override
-    public boolean equals(Object obj){
-        if (this == obj){
-            return true;
-        }
-        if (obj == null){
-            return false;
-        }
-        if (this.getClass() != obj.getClass()){
-            return false;
-        }
-        Star other = (Star) obj;
-        if (this.x != other.x){
-            return false;
-        }
-        if (this.y != other.y){
-            return false;
-        }
-        if (this.z != other.z){
-            return false;
-        } if (this.centerStar != other.centerStar){
-            return false;
-        }
-        return this.name.equals(other.name);
-    }
-    
     @Override
-    public int hashCode(){
-        int hash = 7;
-        hash = 31 * hash + (int) (Double.doubleToLongBits(this.x) ^ (Double.doubleToLongBits(this.x) >>> 32));
-        hash = 31 * hash + (int) (Double.doubleToLongBits(this.y) ^ (Double.doubleToLongBits(this.y) >>> 32));
-        hash = 31 * hash + (int) (Double.doubleToLongBits(this.z) ^ (Double.doubleToLongBits(this.z) >>> 32));
-        hash = 31 * hash + (this.centerStar != null ? this.centerStar.hashCode() : 0);
-        hash = 31 * hash + (this.name != null ? this.name.hashCode() : 0);
-        return hash;
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Planet))
+            return false;
+        if (!super.equals(obj))
+            return false;
+
+        Planet other = (Planet) obj;
+        return Objects.equals(centerStar, other.centerStar);
     }
 
     @Override
-    public String toString(){
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), centerStar);
+    }
+
+    @Override
+    public String toString() {
         double d = CelestialObject.getDistanceBetween(this, centerStar);
-        return String.format("%s circles around %s at the %.3f AU",this.name, centerStar.name, d);
+        return String.format("%s circles around %s at the %.3f AU", name, centerStar.name, d);
     }
 }

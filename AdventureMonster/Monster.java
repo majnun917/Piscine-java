fa59115d6532fbca1ce0c95ce20e97da979d9c59
ndieye <dieye.ndiasse17@gmail.com>
2025-09-29 15:59:29 +0100

    AdventureMonster done!

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,19 @@
+package AdventureMonster;
+
+public class Monster extends Character {
+    public Monster(String name, int maxHealth){
+        super(name, maxHealth);
+    }
+
+    @Override
+    public String toString(){
+        //Monster obj = new Monster(getName(), getMaxHealth());
+        //Character o = new Character(getName(), getMaxHealth());
+        if (this.getCurrentHealth() > 0){
+            return getName() + " is a monster with " + this.getCurrentHealth() + " HP";
+        }
+        return getName() + " is a monster and is dead";
+    }
+
+
+}

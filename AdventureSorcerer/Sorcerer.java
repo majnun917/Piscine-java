677c42f2bd9e467682213e6b2bbef14146cd08ce
ndieye <dieye.ndiasse17@gmail.com>
2025-09-30 10:02:56 +0100

    AdventureSorcerer done!

---
+++ AdventureSorcerer/Sorcerer.java
@@ -0,0 +1,30 @@
+public class Sorcerer extends Character  implements Healer {
+    private int healCapacity;
+
+    public Sorcerer(String name, int maxHealth,  int healCapacity){
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity(){return healCapacity;}
+
+    @Override
+    public void heal(Character hc) {
+        int ch = hc.getCurrentHealth();
+        ch += healCapacity;
+        if (ch > hc.getMaxHealth()) {
+            ch = hc.getMaxHealth();
+        }
+        hc.setCurrentHealth(ch);
+        return;
+    }
+
+   @Override
+   public String toString() {
+       if (getCurrentHealth() == 0) {
+           return getName() + " is a dead sorcerer. So bad, it could heal "+ getHealCapacity()+" HP.";
+       }
+       return getName() + " is a sorcerer with " + getCurrentHealth()+ " HP. " + "It can heal "+ getHealCapacity()+" HP.";
+   }
+}

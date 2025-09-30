a29b73b9020da2940f0195f568f001ebd2f0b960
ndieye <dieye.ndiasse17@gmail.com>
2025-09-30 13:10:15 +0100

    AdventureAbstract done!

--- AdventureAbstract/Monster.java
+++ AdventureAbstract/Monster.java
@@ -1,14 +1,14 @@
 
 public class Monster extends Character {
-    public Monster(String name, int maxHealth){
+    public Monster(String name, int maxHealth) {
         super(name, maxHealth);
     }
 
     @Override
-    public String toString(){
-        //Monster obj = new Monster(getName(), getMaxHealth());
-        //Character o = new Character(getName(), getMaxHealth());
-        if (this.getCurrentHealth() > 0){
+    public String toString() {
+        // Monster obj = new Monster(getName(), getMaxHealth());
+        // Character o = new Character(getName(), getMaxHealth());
+        if (getCurrentHealth() > 0) {
             return getName() + " is a monster with " + this.getCurrentHealth() + " HP";
         }
         return getName() + " is a monster and is dead";
@@ -16,15 +16,14 @@ public class Monster extends Character {
 
     @Override
     public void takeDamage(int amount) {
-       int ad = (int) 0.8 * amount;
-       int newHealth = getCurrentHealth() - ad;
-       if (newHealth < 0) {
-           newHealth = 0;
-       }
-       setCurrentHealth(newHealth);
-       return;
+        int newHealth = getCurrentHealth() - ((int) 0.8 * amount);
+        if (newHealth < 0) {
+            newHealth = 0;
+        }
+        setCurrentHealth(newHealth);
+        return;
     }
-    
+
     @Override
     public void attack(Character c) {
         c.takeDamage(7);

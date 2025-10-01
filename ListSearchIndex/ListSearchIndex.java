2f2395e80861da6897c417af7137f33dbddf7420
ndieye <dieye.ndiasse17@gmail.com>
2025-10-01 12:56:31 +0100

    ListSearch done!

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -1,15 +1,18 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (list == null)
+            return null;
         if (list.lastIndexOf(value) == -1)
             return null;
         return list.lastIndexOf(value);
     }
 
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (list == null)
+            return null;
         if (list.indexOf(value) == -1)
             return null;
         return list.indexOf(value);
@@ -17,6 +20,8 @@ public class ListSearchIndex {
 
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
         ArrayList<Integer> ai = new ArrayList<Integer>();
+        if (list == null)
+            return ai;
         for (int i = 0; i < list.size(); i++) {
             // i = list.indexOf(value);
             if (value.equals(list.get(i))) {

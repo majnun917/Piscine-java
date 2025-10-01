c83bcdc759b5063216330a2c6996cc8b20b27f4c
ndieye <dieye.ndiasse17@gmail.com>
2025-10-01 11:41:22 +0100

    ListContains done!

---
+++ ListContains/ListContains.java
@@ -0,0 +1,10 @@
+package ListContains;
+
+import java.util.List;
+
+public class ListContains {
+     public static boolean containsValue(List<Integer> list, Integer value) {
+        if (list.contains(value)) return true;
+        else return false;
+    }
+}

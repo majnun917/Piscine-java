65e416475143fe30dcb594bc8fab1cea6b6add42
ndieye <dieye.ndiasse17@gmail.com>
2025-10-07 13:12:20 +0100

    Singleton done!

---
+++ Singleton/Excalibur.java
@@ -0,0 +1,22 @@
+public final class Excalibur {
+    private String name;
+    private static Excalibur INSTANCE;
+
+    private Excalibur(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Excalibur getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Excalibur("Sword");
+        }
+        return INSTANCE;
+    }
+    // public static void main(String[] args) {
+    // System.out.println(Excalibur.getInstance().getName());
+    // }
+}
